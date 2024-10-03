90d9e434d26fd12b9419892de7d93084c50c988d
Hemel <reaz.hemel@gmail.com>
2024-10-03 21:50:49 +0600

    updated the video time

--- script/video.js
+++ script/video.js
@@ -1,3 +1,11 @@
+function getTimeString(time){
+  const hour = parseInt(time / 3600)
+  let remainingSecond = parseInt(time % 3600)
+  const minute = (remainingSecond % 60)
+  remainingSecond = parseInt(remainingSecond % 60)
+  return `${hour} hour ${minute} minute ${remainingSecond} seconds ago`
+}
+
 //1 - Fetch, Load and show categories on html
 //create load categories
 const loadCategories = () => {
@@ -30,11 +38,13 @@ const loadVideos = () => {
   const card = document.createElement('div');
   card.classList = 'card card-compact'
   card.innerHTML = `
-  <figure class="h-[200px]">
+  <figure class="h-[200px] relative">
     <img
     class="h-full w-full object-cover"
       src="${video.thumbnail}"
       alt="Shoes" />
+      ${video.others.posted_date?.length === 0 ? "" : `<span class="absolute text-xs right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)}</span>`}
+      
   </figure>
   <div class="px-0 py-2 flex gap-2">
     <div>
@@ -44,7 +54,7 @@ const loadVideos = () => {
     <h2 class="font-bold">${video.title}</h2>
     <div class="flex items-center gap-2">
     <p class="text-gray-400" src="">${video.authors[0].profile_name}</p>
-    <img class="w-5" src ="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>
+    ${video.authors[0].verified === true ? `<img class="w-5" src ="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>` : ""}
     </div>
     
     <p></p>
